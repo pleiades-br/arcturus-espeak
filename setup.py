0ba50846a1b80bbfbd4870bb5015b544ce59b5fb
Aluisio (ZyZy) Victal <alvictal@gmail.com>
2024-08-29 17:29:48 +0000

    feat: First commit adding the initial software

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+    setup tools
+"""
+from setuptools import setup
+setup(
+    name = "String to synthesis wav files ",
+    version = "1.0",
+    packages=["pkg_str2speak"],
+    author="Aluisio Leonello Victal",
+    author_email = "alvictal@gmail.com",
+    description = "Interface for espeak library ",
+    license = "MIT",
+    keywords= "speak, synthesis, libespeak",
+    url = "",
+)
